922b42cb300866784f791c0c4f7a82061126198f
dcz <gilaac.dcz@porcupinefactory.org>
2024-09-08 17:34:46 +0000

    encoding

--- wytros/src/lib.rs
+++ wytros/src/lib.rs
@@ -139,14 +139,14 @@ fn decode_chunk(bits: ReverseBits) -> [u16; 14] {
     out[1] = (bits.get(12, 8) as u16) << 4 | bits.get(20, 4) as u16;
     // 4 independent differential groups in every chunk
     for diffidx in 0..4 {
-        let shift = bits.get(24 + diffidx * (2+3*8), 2);
+        let shift = dh!(bits.get(24 + diffidx * (2+3*8), 2));
         let shift = 4 >> (3 - shift);
         let magnitude = 0x80 << shift;
         // 3 pixels in every group, chained to the previous pixel of the same color
         for pxidx in 0..3 {
             let px_allidx = 2 + diffidx * 3 + pxidx;
             let prev = out[px_allidx - 2];
-            let j = bits.get(24 + 2 + diffidx * (2 + 3 * 8) + pxidx * 8, 8) as u16;
+            let j = dh!(bits.get(24 + 2 + diffidx * (2 + 3 * 8) + pxidx * 8, 8)) as u16;
             let px = if j != 0 {
                 // This is the lossy part. 
                 if (magnitude > prev) | (shift == 4) {
@@ -169,6 +169,26 @@ fn decode_chunk(bits: ReverseBits) -> [u16; 14] {
     out
 }
 
+/// `pxs` is 2 previously encoded pixels + 3 to-be-encoded
+fn calculate_shift(pxs: &[u16]) -> u8 {
+    let mut diffs = [0; 3];
+    dh!(&pxs[..5]);
+    pxs[..3].iter()
+        .zip(pxs[2..][..3].iter())
+        .map(|(p, n)| *p as i16 - *n as i16)
+        .zip(diffs.iter_mut())
+        .for_each(|(d, o)| *o = d);
+    dh!(diffs);
+    let maxdiff = diffs.iter().map(|d| d.abs() as u16).max().unwrap();
+    let magnitude = dh!(dh!(maxdiff).next_power_of_two());
+    match magnitude >> 7 {
+        0 => 0,
+        1 => 1,
+        2 => 2,
+        _ => 4,
+    }
+}
+
 /// Fucking ENCODER. Because otherwise how do I reconstruct the original?
 fn encode_chunk(pxs: &[u16; 14]) -> [u8; 16] {
     let mut bits = ReverseBits([0; 16]);
@@ -181,9 +201,9 @@ fn encode_chunk(pxs: &[u16; 14]) -> [u8; 16] {
     for diffidx in 0..4 {
         let mut outpx = [pxs[diffidx * 3], pxs[diffidx * 3 + 1], 0, 0, 0];
         let mut diffs = [0; 3];
-        dh!(&pxs[diffidx * 3..][..3]);
+        dh!(&pxs[diffidx * 3..][..5]);
         pxs[diffidx * 3..][..3].iter()
-            .zip(pxs[diffidx * 3 + 1..][..3].iter())
+            .zip(pxs[diffidx * 3 + 2..][..3].iter())
             .map(|(p, n)| *p as i16 - *n as i16)
             .zip(diffs.iter_mut())
             .for_each(|(d, o)| *o = d);
@@ -203,7 +223,7 @@ fn encode_chunk(pxs: &[u16; 14]) -> [u8; 16] {
             
         }
         */
-        bits.set(24 + diffidx * (2+3*8), 2, shift);
+        bits.set(24 + diffidx * (2+3*8), 2, dbg!(shift));
     }
     // 3 pixels in every group, chained to the previous pixel of the same color
     /*
@@ -219,25 +239,20 @@ fn compare(a: &[u8; 16], b: &[u8; 16]) {
     let a = ReverseBits(a.clone());
     let b = ReverseBits(b.clone());
     
-    let g = |i, c| assert_eq_hex!(a.get(i, c), b.get(i, c));
+    let g = |i, c, d| assert_eq_hex!(a.get(i, c), b.get(i, c), "test {}", d);
     for diffidx in 0..4 {
-        g(24+diffidx * 2+3*8, 2);
+        g(24+diffidx * (2+3*8), 2, diffidx);
     }
 }
 
 pub fn decode(data: &[u8]) -> Result<Vec<u16>>{
     if data.len() % 0x4000 == 0 {
         let mut out = Vec::with_capacity(data.len() * 14 / 16);
-        iter_chunks(dh!(&data[..0x4000]))
+        iter_chunks(data)
             .enumerate()
             .map(|(i, data)| {
-                if i == 0 {
-                    assert_eq_hex!(
-                        [0x90, 0x7A, 0x8A, 0x18, 0x02, 0x26, 0x92, 0xC7, 0xB7, 0x48, 0x20, 0x1F, 0x20, 0xC6, 0xF0, 0x0B],
-                        data,
-                    );
-                }
-                let bits = ReverseBits(data);
+                dh!(i);
+                let bits = ReverseBits(dh!(data));
                 let out = decode_chunk(bits);
                 compare(&data, &encode_chunk(&out));
                 out
@@ -304,5 +319,24 @@ mod test {
             [0xbf, 0xc6, 0xbf, 0xc2, 0xc0, 0xcd, 0xbc, 0xc6, 0xc5, 0xc6, 0xcb, 0xd0, 0xc5, 0xe0],
             "{:#x?}", &pixels,
         );
+    }
+    
+    #[test]
+    fn reencode() {
+        let ar = [0x90, 0x7A, 0x8A, 0x18, 0x02, 0x26, 0x92, 0xC7, 0xB7, 0x48, 0x20, 0x1F, 0x20, 0xC6, 0xF0, 0x0B];
+        
+        assert_eq!(
+            encode_chunk(&decode_chunk(ReverseBits(ar))),
+            ar,
+        );
+    }
+    
+    #[test]
+    fn enc_diff_shift() {
+        assert_eq!(calculate_shift(&[0xbf, 0xc6, 0xbf, 0xc2, 0xc0][..]), 0);
+        assert_eq!(calculate_shift(&[0xc2, 0xc0, 0xcd, 0xbc, 0xc6][..]), 0);
+        assert_eq!(calculate_shift(&[0xbc, 0xc6, 0xc5, 0xc6, 0xcb][..]), 0);
+        assert_eq!(calculate_shift(&[0xc6, 0xcb, 0xd0, 0xc5, 0xe0][..]), 0);
+        assert_eq!(calculate_shift(&[0x3c1, 0x312, 0x3a9, 0x2f7, 0x3f1][..]), 0);
     }
 }
