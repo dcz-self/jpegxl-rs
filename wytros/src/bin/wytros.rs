b47b36baa6feb36bee071e337a661b0015d6bc47
dcz <gilaac.dcz@porcupinefactory.org>
2024-09-08 17:34:45 +0000

    some stuff

--- wytros/src/bin/wytros.rs
+++ wytros/src/bin/wytros.rs
@@ -1,9 +1,36 @@
+use anyhow::Result;
 use jpegxl_rs as jxl;
+use libopenraw::{rawfile_from_file, Bitmap, Image, RenderingOptions};
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
+use std::env;
+use wytros::decode;
 
 fn main() {
     run().unwrap();
 }
-fn run() -> Result<(), jxl::EncodeError> {
+
+fn run() -> Result<()> {
+    SimpleLogger::new()
+        .with_module_level("libopenraw", LevelFilter::Debug)
+        .init()
+        .unwrap();
+    let path = env::args().skip(1).next().unwrap();
+    if let Ok(rawfile) = rawfile_from_file(path, None) {
+        let img = rawfile.raw_data(false)?;
+        dbg!(img.active_area());
+        dbg!(img.mosaic_pattern());
+        dbg!(img.data_type());
+        dbg!(img.compression());
+        dbg!(img.bpc());
+        dbg!(img.data_size());
+        dbg!(img.width() * img.height());
+        dbg!(img.width());
+        dbg!(img.height());
+        dbg!(img.data8().unwrap().len());
+        dbg!(&img.data8().unwrap()[..16]);
+        dbg!(decode(&img.data8().unwrap()[..16])?);
+    }
     let mut enc = jxl::encoder_builder()
         // we're compressing raw, duh
         .lossless(true)
