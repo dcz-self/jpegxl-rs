4efc6aec4b2f565b1e6ae68da01dd393c65b816e
dcz <gilaac.dcz@porcupinefactory.org>
2024-09-08 17:34:45 +0000

    wytros builds

--- wytros/src/bin/wytros.rs
+++ wytros/src/bin/wytros.rs
@@ -3,16 +3,16 @@ use jpegxl_rs as jxl;
 fn main() {
     run().unwrap();
 }
-fn run() -> Result<(), ()> {
+fn run() -> Result<(), jxl::EncodeError> {
     let mut enc = jxl::encoder_builder()
         // we're compressing raw, duh
         .lossless(true)
         .uses_original_profile(true)
-        .speed(jxl::EncoderSpeed::Tortoise)
+        .speed(jxl::encode::EncoderSpeed::Tortoise)
         .use_container(true)
         // not really true for raw sensor data but doesn't hurt I guess.
         // I don't know what it changes apart from color profile in metadata anyway
-        .color_encoding(jxl::ColorEncoding::LinearSRGB)
+        .color_encoding(jxl::encode::ColorEncoding::LinearSrgb)
         .build()?;
-    
+    Ok(())
 }
