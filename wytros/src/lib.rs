b5bed0e71a9760cf5613bec5102e09c8887c6f75
dcz <gilaac.dcz@porcupinefactory.org>
2024-09-08 17:34:45 +0000

    wip reconstructing chunk decoder

--- wytros/src/lib.rs
+++ wytros/src/lib.rs
@@ -25,6 +25,12 @@ fn block_get_chunk(data: &[u8], chunk_idx: usize) -> [u8; 16] {
     out
 }
 
+macro_rules! to_lsb_mask {
+    ($count:expr) => {
+        !(!0 << count)
+    };
+}
+
 struct ReverseBits([u8;16]);
 
 impl ReverseBits {
@@ -37,6 +43,27 @@ impl ReverseBits {
         let bit_offset = bit_index % 8;
         let mask = !(!0u16 << count) as u8;
         (data >> bit_offset) as u8 & mask
+    }
+}
+
+fn decode_chunk(bits: ReverseBits) -> [u16; 14] {
+    let mut out = [0; u16];
+    out[0] = bits.get(0, 8) << 4 | bits.get(8, 4);
+    out[1] = bits.get(12, 8) << 4 | bits.get(20, 4);
+    for diffidx in 0..4 {
+        let shift = 4 >> (3 - bits.get(24 + diffidx * 18, 2));
+        let magnitude = 0x80 << shift;
+        for pxidx in 0..3 {
+            let px_allidx = 2 + diffidx * 3 + pxidx;
+            let prev = out[px_allidx - 2];
+            let j = bits.get(24 + diffidx * 18 + pxidx * 8, 8) as u16;
+            let px = if magnitude > prev | shift == 4 {
+                j << shift | prev & !(!0 << shift)
+            } else {
+                prev - magnitude + j
+            };
+            out[px_allidx] = px;
+        }
     }
 }
 
